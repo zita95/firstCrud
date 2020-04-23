6600778daafb017778cd44d21fe601eb08518b1f
Youngcatlady95 <szizitaa@gmail.com>
2020-04-23 09:47:52 +0200

    added groups handlers

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,15 @@ interface User {
   lastName: string;
   email: string;
   age: number;
-  created_at: string;
-  updated_at: string;
+  created_at?: string;
+  updated_at?: string;
 }
 
-app.get('/', async (req: Request, res: Response) => {
+/* app.get('/', async (req: Request, res: Response) => {
   res.json({
     message: 'hello world',
   });
-});
+}); */
 
 app.get('/user', async (req: Request, res: Response) => {
   const users: Array<User> = await database('users').select();
@@ -85,7 +85,87 @@ app.put('/user/:id', async (req: Request, res: Response) => {
     console.error(error);
     res.sendStatus(500);
   }
-})
+});
+
+interface Group {
+  id: number;
+  name: string;
+  description: string;
+  location: string;
+  maxSize: number;
+};
+
+app.get('/group', async (req: Request, res: Response) => {
+  try {
+    const groups: Array<Group> = await database('groups').select();
+    res.json(groups);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.post('/group', async (req: Request, res: Response) => {
+  try {
+    const group: Group = {
+      name: req.body.name,
+      description: req.body.description,
+      location: req.body.location,
+      maxSize: req.body.size
+    };
+    await database('groups').insert(group);
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.get('/group/:id', async (req: Request, res: Response) => {
+  try {
+    const result = await database('groups').select().where({ id: req.params.id }).first();
+    if (result) {
+      res.json(result);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.delete('/group/:id', async (req: Request, res: Response) => {
+  try {
+    const result = await database('groups').select().where({ id: req.params.id });
+    if (result) {
+      await database('groups').where({ id: req.params.id }).del();
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.put('/group/:id', async (req: Request, res: Response) => {
+  try {
+    const result = await database('groups').select().where({ id: req.params.id });
+    const newResult: Group = {
+      name: req.body.name,
+      description: req.body.description,
+      location: req.body.location,
+      maxSize: req.body.size
+    };
+    await database('groups').update(newResult).where({ id: req.params.id });
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
